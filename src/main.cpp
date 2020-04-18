63c669ba6b566df652f1e11ce9cad7c9c8ec3c6a
Tim Lane <timlanesoftware@gmail.com>
2020-04-18 11:47:20 +0100

    Added world map

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,36 @@ using namespace Ossium;
 
 int main(int argc, char* argv[])
 {
-
     InitialiseOssium();
 
+    InputController input;
+    InputContext mainContext;
 
+    input.AddContext("window_context", &mainContext);
+
+    Window* window = mainContext.AddHandler<Window>();
+    window->Init("Network Trouble", 1280, 768);
+
+    Renderer renderer(window, 100);
+    ResourceController resources;
+
+    ServicesProvider services(&renderer, &resources, &input);
+
+    EngineSystem engine(&services);
+
+    Image bg;
+    bg.Load("assets/world_map.png");
+    bg.PushGPU(renderer, SDL_TEXTUREACCESS_STATIC);
+
+    Texture* background = engine.GetScene()->CreateEntity()->AddComponent<Texture>();
+    background->SetSource(&bg);
+    background->GetTransform()->SetWorldPosition(Vector2(1280 / 2, 768 / 2));
+
+    while (engine.Update())
+    {
+    }
 
     TerminateOssium();
 
+    return 0;
 }
